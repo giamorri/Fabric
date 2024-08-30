616b177d798a382b3f8c2f026a32428aff680497
ZenZuki <99848533+ZenZuki@users.noreply.github.com>
2024-08-30 13:56:35 +1200

    trying to fix stuff

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
