1250d35b9a221463dfa164adb206782ad205ed17
ZenZuki <99848533+ZenZuki@users.noreply.github.com>
2024-08-30 21:51:22 +1200

    tried to make design less ugly (cant read the text, but im changing the font trust)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import sampleLogo from './images/logoSample3.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <ul className="fabricSample">
+        <img src={sampleLogo}></img>
+      </ul>
       <ul className="navbar-list">
         <li><Link to="/Home">Home</Link></li>
         <li><Link to="/Profile">Profile</Link></li>
