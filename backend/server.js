3924b813e9e69aa7bb7921dd1cc1008337edc541
Leen <132303487+Leenk0@users.noreply.github.com>
2024-09-20 15:57:48 +1200

    new

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors'); // To allow requests from React frontend
 const app = express();
-const port = 5001;
+const port = 5000;
 const multer = require('multer');
 const path = require('path');
 
