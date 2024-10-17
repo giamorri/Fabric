d5b230624c2be191bb1e384be08afe8673b2fe01
Leen <132303487+Leenk0@users.noreply.github.com>
2024-10-17 14:20:30 +1300

    fixing

---
+++ .vscode/config-overrides.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "zlib": require.resolve("browserify-zlib"),
+    "util": require.resolve("util/"),
+    "url": require.resolve("url/"),
+    "assert": require.resolve("assert/")
+  };
+
+  return config;
+};
